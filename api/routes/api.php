531528121bbe5035aaede5d5377778423ccd6560
Ricardo Grana <rickgrana@gmail.com>
2024-06-03 10:59:44 -0400

    fix: changed Task API to be accesed only by authenticated

--- api/routes/api.php
+++ api/routes/api.php
@@ -13,13 +13,14 @@ Route::group([
     Route::post('auth/register', [RegisterController::class, 'register']);
     Route::post('auth/login', 'AuthController@login');
     Route::post('auth/logout', 'AuthController@logout');
-    //Route::post('refresh', 'AuthController@refresh');
-
-
     Route::post('me', 'AuthController@me');
-    Route::apiResource('/tasks', TaskController::class);
-});
 
+    Route::group([
+        "middleware" => "auth"
+    ], function ($router) {
+        Route::apiResource('/tasks', TaskController::class);
+    });
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
