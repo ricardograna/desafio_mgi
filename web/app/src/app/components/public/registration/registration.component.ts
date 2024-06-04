dafa02e7fa6af02f1062f793bbedc88385de907c
Ricardo Grana <rickgrana@gmail.com>
2024-06-04 10:35:21 -0400

    enh: Added Material components to Registration Form

--- web/app/src/app/components/public/registration/registration.component.ts
+++ web/app/src/app/components/public/registration/registration.component.ts
@@ -2,19 +2,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatCardModule, MatButtonModule],
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent {
   registrationForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  constructor(private fb: FormBuilder, private router: Router, private authService: AuthService) {
     this.registrationForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
@@ -35,6 +41,7 @@ export class RegistrationComponent {
       this.authService.register(this.registrationForm.value)
         .subscribe(response => {
           console.log('Success!', response);
+          this.router.navigateByUrl('/login');
         }, error => {
           console.error('Error!', error);
         });
